a66a925a484749f133614b094c56fadbe54a03f3
PhamThiLanAnh <anhphamnb1002@gmail.com>
2023-08-14 01:18:48 +0700

    apdate

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,12 +1,34 @@
-﻿#include <GL/glut.h>
+﻿#include <iostream>
+using namespace std;
+#include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <cmath>
+GLfloat teta = 0;
 
-float x = 1.0;
-float y = 2.0;
-float z = 1.0;
+float x = 0.0;
+float y = 0.0;
+float z = 0.0;
+float headRadius = 0.5;
 const float PI = 3.14159;
+
+
+void truc()
+{
+	glBegin(GL_LINES);
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(100.0, 0.0, 0.0);
+	glVertex3f(0.0, 0.0, 0.0);
+
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(0.0, 100.0, 0.0);
+	glVertex3f(0.0, 0.0, 0.0);
+
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(0.0, 0.0, 100);
+	glVertex3f(0.0, 0.0, 0.0);
+	glEnd();
+}
 
 void drawArc(float startX, float startY, float startZ, float radius, float startAngle, float endAngle, int numSegments, float increaseZ = 0) {
 	glBegin(GL_LINE_STRIP);
@@ -18,22 +40,51 @@ void drawArc(float startX, float startY, float startZ, float radius, float start
 	}
 	glEnd();
 }
-void truc()
-{
+
+void drawWing(float centerX, float centerY, float centerZ, float a, float b, int numPoints) {
+	glBegin(GL_POINTS);
+	for (int i = 0; i < numPoints; ++i) {
+		float angle = 2.0 * PI * static_cast<float>(i) / static_cast<float>(numPoints);
+		float x = centerX + a * cos(angle);
+		float y = centerY + b * sin(angle);
+		glVertex3f(x, y, centerZ);
+	}
+	glEnd();
+
+}
+
+//void drawLine(float x1, float y1, float z1, float x2, float y2, float z2, int numPoints) {
+//	glBegin(GL_POINTS);
+//
+//	float dx = x2 - x1;
+//	float dy = y2 - y1;
+//	float dz = z2 - z1;
+//
+//	float xIncrement = dx / numPoints;
+//	float yIncrement = dy / numPoints;
+//	float zIncrement = dz / numPoints;
+//
+//	float x = x1;
+//	float y = y1;
+//	float z = z1;
+//
+//	for (int i = 0; i <= numPoints; ++i) {
+//		glVertex3f(x, y, z);
+//		x += xIncrement;
+//		y += yIncrement;
+//		z += zIncrement;
+//	}
+//
+//	glEnd();
+//}
+
+void drawLine(float x1, float y1, float z1, float x2, float y2, float z2) {
 	glBegin(GL_LINES);
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(50.0, 0.0, 0.0);
-	glVertex3f(0.0, 0.0, 0.0);
-
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(0.0, 50.0, 0.0);
-	glVertex3f(0.0, 0.0, 0.0);
-
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(0.0, 0.0, 50);
-	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(x1, y1, z1);
+	glVertex3f(x2, y2, z2);
 	glEnd();
 }
+
 
 void MyDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT); //xóa bộ đệm màu| xóa chiều sâu trục z k bị chồng
@@ -43,35 +94,40 @@ void MyDisplay() {
 
 	truc();
 
+	glutPostRedisplay();
 	//////////// đầu
 
 	glColor3f(0.9, 0.8, 0.0);
+	//glutSolidSphere(0.5, 20, 20);
 	glutWireSphere(0.5, 20, 20);
 
 	////////////// thân1 ////////////
 	
 	
-	glPopMatrix();
-	glPushMatrix();
-	glTranslatef(0.95, 0.0, 0.0);//tinh tien
-	glScalef(0.9, 0.7, 0.7);//co dan
-	//glTranslatef(1.25, 0.0, 0.0);
-	glutWireSphere(0.5, 20, 20);
+	//glPopMatrix();
+	//glPushMatrix();
+	//glTranslatef(0.95, 0.0, 0.0);//tinh tien
+	//glScalef(0.9, 0.7, 0.7);//co dan
+	////glTranslatef(1.25, 0.0, 0.0);
+	//glutWireSphere(0.5, 20, 20);
 
 	///////////// thân2 ////////////
 	
+	glColor3f(1.0, 0.8, 0.6);
 	glPopMatrix();
 	glPushMatrix();
-	glTranslatef(2.65, 0.0, 0.0);
-	glScalef(2.55, 0.7, 1.0);//co dan
+	glTranslatef(1.7, 0.0, 0.0);
+	glScalef(3.0, 1.0, 1.0);//co dan
 	glutWireSphere(0.5, 20, 20);
+	//glutSolidSphere(0.5, 20, 20);
 
 	/////////// Mắt phải to////////////
 
 	
 	glPopMatrix();
 	glPushMatrix();
-	glColor3f(1.0, 1.0, 1.0);
+
+	glColor3f(1.0, 0.9, 0.6);
 	glRotatef(20, 1.0, 0.0, 0.0);
 	glTranslatef(-0.4, 0.2, 0.2);
 	glScalef(0.35, 0.45, 0.35);//co dan	
@@ -80,6 +136,7 @@ void MyDisplay() {
 	/////////// Mắt phải bé////////////
 	glPopMatrix();
 	glPushMatrix();
+
 	glColor3f(0.0, 0.0, 0.0);
 	glRotatef(20, 1.0, 0.0, 0.0);
 	glTranslatef(-0.5, 0.15, 0.2);
@@ -92,7 +149,8 @@ void MyDisplay() {
 	
 	glPopMatrix();
 	glPushMatrix();
-	glColor3f(1.0, 1.0, 1.0);
+
+	glColor3f(1.0, 0.9, 0.6);
 	glRotatef(20, -1.0, 0.0, 0.0);
 	glTranslatef(-0.4, 0.2, -0.2);
 	glScalef(0.35, 0.45, 0.35);//co dan
@@ -101,6 +159,7 @@ void MyDisplay() {
 	/////////// Mắt trái bé////////////
 	glPopMatrix();
 	glPushMatrix();
+
 	glColor3f(0.0, 0.0, 0.0);
 	glRotatef(20, -1.0, 0.0, 0.0);
 	glTranslatef(-0.5, 0.15, -0.2);
@@ -111,94 +170,283 @@ void MyDisplay() {
 
 	glPopMatrix();
 	glPushMatrix();
+
 	glColor3f(0.9, 0.0, 0.8);
 	glRotatef(90, 1.0, 0.0, 0.0);
-	glTranslatef(-0.35, 0.0, 0.2);
+	glTranslatef(-0.34, 0.0, 0.2);
 	glScalef(0.3, 0.3, 0.3);//co dan
 	//glutSolidSphere(0.5, 100, 100);
 	glutSolidCone(0.5, 0.5, 20, 20);
 
 	/////////// Râu phải ////////////
+	glPopMatrix();
+	glPushMatrix();
+
+	glRotatef(90, 0.0, -1.0, 0.0);
+	//glTranslatef(0.0, 0.6, -0.4);
+	////drawArc(0.0 + 0.5 * 1.2, 0.5 * 0.8, 0.5 / 2, 0.5, PI, PI / 2, 50, 0.01);
+	//drawArc(0.0, 0.0, 0.0, 0.5, PI, 2*PI/2, 50, 0.01);
+	glTranslatef(-0.7, 0.4, 0.0);
+	drawArc(0.0, 0.0, 0.0, 0.5, 0, 0.5 * PI / 0.75, 50, 0.01);
+
 
 	/////////// Râu trái ////////////
-	
-	glPushMatrix();
-	glRotatef(145, 0, 1, 0);
-	glTranslatef(-0.35, 0.0, 0.2);
-	glScalef(-1.0, 1.0, 1.0);
-	drawArc(x - y * 0.5, y * 0.5, y / 0.5, y, 0.5, PI / 1.0, 40, 0.5);
 	glPopMatrix();
-	/////////// Chân phải 1 ////////////
+	glPushMatrix();
 
-	/////////// Chân trái 1 ////////////
+	glRotatef(90, 0.0, 1.0, 0.0);
+	glTranslatef(-0.7, 0.4, 0.0);
+	drawArc(0.0, 0.0, 0.0, 0.5, 0, 0.5 * PI / 0.75, 50, 0.01);
+	
 
+	/////////// Chân phải trên 1 ////////////
+	/*glPopMatrix();
+	glPushMatrix();
+	glTranslatef(2.0, 0.0, 0.0);
+	glRotatef(45, 1.0, 0.0, 0.0);
+	glRotatef(150, 0.0, 1.0, 0.0);
+	glScalef(1.0, 0.05, 0.05);
+	glutSolidCube(1.0);
+
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(1.05, -0.85, -0.85);
+	glRotatef(-45, 1.0, 0.0, 0.0);
+	glRotatef(-150, 0.0, 1.0, 0.0);
+	glScalef(1.0, 0.05, 0.05);
+	glutSolidCube(0.5);*/
+
+	
+
+	/////////// Chân phải dưới 1 ////////////
+	/*glPopMatrix();
+	glPushMatrix();
+	glTranslatef(1.73, -1.6, 1.0);
+	glRotatef(30, 0.0, 0.0, 1.0);
+	glScalef(0.05, 1.3, 0.05);
+	glutSolidCube(1.0);
+
+	glPopMatrix();
+	glPushMatrix();
+
+	glTranslatef(1.73, -1.6, -1.0);
+	glRotatef(30, 0.0, 0.0, 1.0);
+	glScalef(0.05, 1.3, 0.05);
+	glutSolidCube(1.0);*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/////////// Chân phải trên 1 ////////////
+
+
+	/*glColor3f(0.0, 0.0, 0.0); 
+	glPopMatrix();
+	glPushMatrix();
+
+	glRotatef(90, 1.0, 0.0, 0.0);
+	glTranslatef(0.5, 0.0, 0.0);
+	drawLine(0.0, 0.0, 0.0, 0.0, 0.5, 0.5);*/
+
+	///////////// Chân phải dưới 1 ////////////
+
+	//glPopMatrix();
+	//glPushMatrix();
+
+	////glRotatef(-90, 1.0, 0.0, 0.0);
+	//glTranslatef(0.5, -1.0, 0.0);
+	//draw3DLine(0.0, 0.0, 0.0, 0.0, 0.5, 0.5);
+	/////////////////////////////////////////////////////////////////////////////////////////////
+
+	///////////// Chân phải trên 1 ////////////
+
+	glColor3f(0.0, 0.0, 0.0);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.7, -0.2, 0.25);
+	glRotatef(130, 0.0, -1.0, 0.0);
+	drawLine(0.0, 0.0, 0.0, 0.7, 0.0, 0.0);
+
+	///////////// Chân phải dưới 1 ////////////
+
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.3, -0.2, 0.75);
+	glRotatef(-120, 0.0, 0.0, 1.0);
+	drawLine(0.0, 0.0, 0.0, 0.4, 0.0, 0.0);
+
+	///////////// Chân phải trên 2 ////////////
+
+	glColor3f(0.0, 0.0, 0.0);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(1.4, -0.2, 0.25);
+	glRotatef(130, 0.0, -1.0, 0.0);
+	drawLine(0.0, 0.0, 0.0, 0.7, 0.0, 0.0);
+
+	///////////// Chân phải dưới 2 ////////////
+
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(1.0, -0.2, 0.75);
+	glRotatef(-120, 0.0, 0.0, 1.0);
+	drawLine(0.0, 0.0, 0.0, 0.4, 0.0, 0.0);
+
+	///////////// Chân phải trên 3 ////////////
+
+	glColor3f(0.0, 0.0, 0.0);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(2.1, -0.2, 0.25);
+	glRotatef(130, 0.0, -1.0, 0.0);
+	drawLine(0.0, 0.0, 0.0, 0.7, 0.0, 0.0);
+
+	///////////// Chân phải dưới 3 ////////////
+
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(1.7, -0.2, 0.75);
+	glRotatef(-120, 0.0, 0.0, 1.0);
+	drawLine(0.0, 0.0, 0.0, 0.4, 0.0, 0.0);
+
+
+	/////////// Chân trái trên 1 ////////////
+
+	glColor3f(0.0, 0.0, 0.0);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.7, -0.2, -0.25);
+	glRotatef(130, 0.0, 1.0, 0.0);
+	drawLine(0.0, 0.0, 0.0, 0.7, 0.0, 0.0);
+
+	/////////// Chân trái dưới 1 ////////////
+
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.3, -0.2, -0.75);
+	glRotatef(-120, 0.0, 0.0, 1.0);
+	drawLine(0.0, 0.0, 0.0, 0.4, 0.0, 0.0);
+
+
+	/////////// Chân trái trên 2 ////////////
+
+	glColor3f(0.0, 0.0, 0.0);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(1.4, -0.2, -0.25);
+	glRotatef(130, 0.0, 1.0, 0.0);
+	drawLine(0.0, 0.0, 0.0, 0.7, 0.0, 0.0);
+
+	/////////// Chân trái dưới 2 ////////////
+
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0.95, -0.2, -0.75);
+	glRotatef(-120, 0.0, 0.0, 1.0);
+	drawLine(0.0, 0.0, 0.0, 0.4, 0.0, 0.0);
+
+
+	/////////// Chân trái trên 3 ////////////
+
+	glColor3f(0.0, 0.0, 0.0);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(2.1, -0.2, -0.25);
+	glRotatef(130, 0.0, 1.0, 0.0);
+	drawLine(0.0, 0.0, 0.0, 0.7, 0.0, 0.0);
+
+	/////////// Chân trái dưới 3 ////////////
+
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(1.7, -0.2, -0.75);
+	glRotatef(-120, 0.0, 0.0, 1.0);
+	drawLine(0.0, 0.0, 0.0, 0.4, 0.0, 0.0);
 
 	/////////// cánh phải 1////////////
 
-	//glColor3f(1.0, 0.0, 0.2);
-	//glPopMatrix();
-	//glPushMatrix();
-	//glRotatef(40, -1, 0, 0);
+	glColor3f(0.9 , 0.5 ,0.1);
+	glPopMatrix();
+	glPushMatrix();
+	glRotatef(40, -1, 0, 0);
+	glTranslatef(0.0, 0.0, 0.0);
+	glScalef(1.5, 1.5, 3.0);//co dan
+	glTranslatef(0.5, 0.0, 0.5);
+	glutSolidSphere(0.5, 20, 20);
+
+	//glColor3f(0.0, 0.0, 0.0);
+	////glPopMatrix();
+	////glPushMatrix();
+	//glRotatef(40, 0, 0, -1);
 	//glTranslatef(0.0, 0.0, 0.0);
-	//glScalef(2.5, 1.0, 4.0);//co dan
-	//glTranslatef(0.3, 0.0, 0.5);
+	//glScalef(1.5, 1.5, 2.75);//co dan
+	//glTranslatef(0.7, 0.0, 0.5);
 	//glutWireSphere(0.5, 20, 20);
-	//
+	
 
-	///////////// cánh trái 1 ////////////
+	/////////// cánh trái 1 ////////////
 
-	//glPopMatrix();
-	//glPushMatrix();
-	//glRotatef(40, 1, 0, 0);
-	//glTranslatef(0.0, 0.0, 0.0);
-	//glScalef(2.5, 1.0, -4.0);//co dan
-	//glTranslatef(0.3, 0.0, 0.5);
-	//glutWireSphere(0.5, 20, 20);
+	glPopMatrix();
+	glPushMatrix();
+	glRotatef(40, 1, 0, 0);
+	glTranslatef(0.0, 0.0, 0.0);
+	glScalef(1.5, 1.5, -3.0);//co dan
+	glTranslatef(0.5, 0.0, 0.5);
+	glutSolidSphere(0.5, 20, 20);
 
-	///////////// cánh phải 2////////////
+	/////////////// cánh phải 2////////////
 
-	//glColor3f(0.8, 0.4, 1.0);
-	//glPopMatrix();
-	//glPushMatrix();
-	//glRotatef(50, -1, 0, 0);
-	//glTranslatef(0.0, 0.0, 0.0);
-	//glScalef(2.5, 1.0, 4.0);//co dan
-	//glTranslatef(0.3, 0.0, 0.5);
-	//glutWireSphere(0.5, 20, 20);
+	glColor3f(0.9, 0.7, 0.5);
+	glPopMatrix();
+	glPushMatrix();
+	glRotatef(40, -1, 0, 0);
+	glTranslatef(0.0, 0.0, 0.0);
+	glScalef(2.2, 1.5, 2.5);//co dan
+	glTranslatef(0.75, 0.0, 0.5);
+	glutSolidSphere(0.5, 20, 20);
 
 
 	///////////// cánh trái 2////////////
 
-	//glPopMatrix();
-	//glPushMatrix();
+	glPopMatrix();
+	glPushMatrix();
+	glRotatef(40, 1, 0,0);
+	glTranslatef(0.0, 0.0, 0.0);
+	glScalef(1.5, 1.5, -2.0);//co dan
 	//glRotatef(0, 1, 0, 0);
-	//glTranslatef(0.0, 0.0, 0.0);
-	//glScalef(2.5, 1.0, -4.0);//co dan
-	//glTranslatef(0.3, 0.0, 0.5);
-	//glutWireSphere(0.5, 20, 20);
+	glTranslatef(0.75, 0.0, 0.5);
+	glutSolidSphere(0.5, 20, 20);
 
 
-	///////////// cánh phải 2////////////
+	/////////////// cánh phải 3////////////
 
-	//glColor3f(0.8, 0.4, 1.0);
+	//glColor3f(0.7, 0.4, 1.0);
 	//glPopMatrix();
 	//glPushMatrix();
-	//glRotatef(50, -1, 0, 0);
+	//glRotatef(-65, 1, 0, 0);
 	//glTranslatef(0.0, 0.0, 0.0);
-	//glScalef(2.5, 1.0, 4.0);//co dan
-	//glTranslatef(0.3, 0.0, 0.5);
+	//glScalef(0.5, 0.5, 4.0);//co dan
+	//glTranslatef(0.7, 0.0, 0.5);
 	//glutWireSphere(0.5, 20, 20);
 
 
-	///////////// cánh trái 2////////////
+	///////////// cánh trái 3////////////
 
 	//glPopMatrix();
 	//glPushMatrix();
-	//glRotatef(0, 1, 0, 0);
+	//glRotatef(65, 1, 0, 0);
 	//glTranslatef(0.0, 0.0, 0.0);
-	//glScalef(2.5, 1.0, -4.0);//co dan
-	//glTranslatef(0.3, 0.0, 0.5);
+	//glScalef(0.5, 0.5, -4.0);//co dan
+	//glTranslatef(0.7, 0.0, 0.5);
 	//glutWireSphere(0.5, 20, 20);
+
+	//glPopMatrix();
+	//glPushMatrix();
+	//glColor3f(1.0f, 0.0f, 0.0f);
+	//glRotatef(90, 0, 1, 1);
+	////glTranslatef(0.0, 0.0, 0.0);
+	//glScalef(0.5, 0.5, -4.0);
+	//glTranslatef(0.0, 2.0, 0.5);
+	//drawWing(0.0, 0.0, 0.0, 4.0, 4.0, 200);
 
 	glFlush();
 	
@@ -213,11 +461,10 @@ void reshape(int w, int h)
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 	glMatrixMode(GL_PROJECTION);//tbao su mt chieu
 	glLoadIdentity();
-	//glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 	gluPerspective(45,(GLfloat) w/(GLfloat )h, 1, 200);//ma tran chieu
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(2.0, 5, 10.0, 0, 0, 0, 0, 1, 0);// nhin truc z=10, cho trục y lên trên x=0, y=1, z=0
+	gluLookAt(-10.0, 2.0, 7.0, 0, 0, 0, 0, 1, 0);// nhin truc z=10, cho trục y lên trên x=0, y=1, z=0
 
 }
 
@@ -227,6 +474,7 @@ int main(int argc, char** argv) {
 	glutInitWindowSize(700, 700);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("OpenGL Sample Drawing");
+	glEnable(GL_DEPTH_TEST);
 
 	init();
 	glutDisplayFunc(MyDisplay);
